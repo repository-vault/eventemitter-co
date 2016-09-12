df4986e50a0b1e41a9cd3a1acdd3cc23edc3bff3
François Leurent <131.php@cloudyks.org>
2016-09-12 19:31:36 +0200

    Should always return a promise

--- index.js
+++ index.js
@@ -25,11 +25,12 @@ const EventEmitter = new Class({
 
   emit:function(event, payload){
     if(!this.callbacks[event])
-      return;
+      return Promise.resolve();
+
+    var chain = [];
 
     var args = Array.prototype.slice.call(arguments, 1);
 
-    var chain = [];
     forIn(this.callbacks[event], function(callback) {
       var p = co.apply(callback.ctx, [callback.callback].concat(args));
       chain.push(p);
